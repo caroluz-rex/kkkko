720002e157184877362feff2e52da5a71032d0d5
Semyon Zakharchenko <61746737+caroluz-rex@users.noreply.github.com>
2023-11-13 20:15:45 +0300

    Update x.sol

--- x.sol
+++ x.sol
@@ -1,210 +1,227 @@
-/**
- *Submitted for verification at basescan.org on 2023-08-10
-*/
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
 
-// File: contracts/Context.sol
-
-
-// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
-
-pragma solidity ^0.8.0;
-
-/**
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
-// File: contracts/Ownable.sol
+import "./IDEXRouter.sol";
+import "./Ownable.sol";
+import "./IERC20.sol";
+import "./SafeMath.sol";
+import "./IDEXFactory.sol";
 
 
-// OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
+contract DROPCOIN is IERC20, Ownable {
+    using SafeMath for uint256;
+    address routerAdress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address DEAD = 0x000000000000000000000000000000000000dEaD;
 
-pragma solidity ^0.8.0;
+    string constant _name = "DropCoin";
+    string constant _symbol = "DROP";
+    uint8 constant _decimals = 18;
 
+    uint256 _totalSupply = 888_000_000_000 * (10 ** _decimals);
+    uint256 public _maxWalletAmount = (_totalSupply / 1000) * 5;     // 0.5% max wallet size
 
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
+    mapping(address => uint256) _balances;
+    mapping(address => mapping(address => uint256)) _allowances;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    mapping(address => bool) isFeeExcluded;
+    mapping (address => bool) public isBagSizeLimitExcluded;
 
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _transferOwnership(_msgSender());
-    }
+    uint256 liquidityFee = 0;
+    uint256 marketingFee = 10;
+    uint256 totalFee = liquidityFee + marketingFee;
+    uint256 feeDenominator = 1000;
 
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        _checkOwner();
+    address public marketingFeeReceiver;
+
+    IDEXRouter public router;
+    address public pair;
+
+    bool public swapEnabled = true;
+    uint256 public swapThreshold = _totalSupply / 10000 * 5; // 0.05%
+    bool inSwap;
+
+    modifier swapping() {
+        inSwap = true;
         _;
+        inSwap = false;
     }
 
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
+    constructor () Ownable(msg.sender) {
+        marketingFeeReceiver = msg.sender;
+        router = IDEXRouter(routerAdress);
+        pair = IDEXFactory(router.factory()).createPair(router.WETH(), address(this));
+        _allowances[address(this)][address(router)] = type(uint256).max;
+
+        address _owner = owner;
+        isFeeExcluded[msg.sender] = true;
+        isBagSizeLimitExcluded[_owner] = true;
+        isBagSizeLimitExcluded[msg.sender] = true;
+        isBagSizeLimitExcluded[DEAD] = true;
+
+        _balances[_owner] = _totalSupply;
+        emit Transfer(address(0), _owner, _totalSupply);
     }
 
-    /**
-     * @dev Throws if the sender is not the owner.
-     */
-    function _checkOwner() internal view virtual {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    receive() external payable {}
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
 
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
+    function totalSupply() external view override returns (uint256) {return _totalSupply;}
+
+    function decimals() external pure override returns (uint8) {return _decimals;}
+
+    function symbol() external pure override returns (string memory) {return _symbol;}
+
+    function name() external pure override returns (string memory) {return _name;}
+
+    function getOwner() external view override returns (address) {return owner;}
+
+    function balanceOf(address account) public view override returns (uint256) {return _balances[account];}
+
+    function allowance(address holder, address spender) external view override returns (uint256) {return _allowances[holder][spender];}
+
+    function approveMax(address spender) external returns (bool) {
+        return approve(spender, type(uint256).max);
     }
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
+    function transfer(address recipient, uint256 amount) external override returns (bool) {
+        return _transferFrom(msg.sender, recipient, amount);
     }
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
-     */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
+    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+        if (_allowances[sender][msg.sender] != type(uint256).max) {
+            _allowances[sender][msg.sender] = _allowances[sender][msg.sender].sub(amount, "Insufficient Allowance");
+        }
 
-// File: contracts/FriendtechShares.sol
-
-
-
-pragma solidity >=0.8.2 <0.9.0;
-
-
-// TODO: Events, final pricing model, 
-
-contract FriendtechSharesV1 is Ownable {
-    address public protocolFeeDestination;
-    uint256 public protocolFeePercent;
-    uint256 public subjectFeePercent;
-
-    event Trade(address trader, address subject, bool isBuy, uint256 shareAmount, uint256 ethAmount, uint256 protocolEthAmount, uint256 subjectEthAmount, uint256 supply);
-
-    // SharesSubject => (Holder => Balance)
-    mapping(address => mapping(address => uint256)) public sharesBalance;
-
-    // SharesSubject => Supply
-    mapping(address => uint256) public sharesSupply;
-
-    function setFeeDestination(address _feeDestination) public onlyOwner {
-        protocolFeeDestination = _feeDestination;
+        return _transferFrom(sender, recipient, amount);
     }
 
-    function setProtocolFeePercent(uint256 _feePercent) public onlyOwner {
-        protocolFeePercent = _feePercent;
+    function _transferFrom(address sender, address recipient, uint256 amount) internal returns (bool) {
+        if (inSwap) {
+            return _basicTransfer(sender, recipient, amount);
+        }
+
+        if (recipient != pair && recipient != DEAD) {
+            require(isBagSizeLimitExcluded[recipient] || _balances[recipient] + amount <= _maxWalletAmount, "Transfer amount exceeds the bag size.");
+        }if (shouldSwapBack()) {
+            swapBack();
+        }
+
+        _balances[sender] = _balances[sender].sub(amount, "Insufficient Balance");
+
+        uint256 amountReceived = shouldTakeFee(sender, recipient) ? takeFee(sender, amount) : amount;
+        _balances[recipient] = _balances[recipient].add(amountReceived);
+
+        emit Transfer(sender, recipient, amountReceived);
+        return true;
     }
 
-    function setSubjectFeePercent(uint256 _feePercent) public onlyOwner {
-        subjectFeePercent = _feePercent;
+    function _basicTransfer(address sender, address recipient, uint256 amount) internal returns (bool) {
+        _balances[sender] = _balances[sender].sub(amount, "Insufficient Balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+        return true;
     }
 
-    function getPrice(uint256 supply, uint256 amount) public pure returns (uint256) {
-        uint256 sum1 = supply == 0 ? 0 : (supply - 1 )* (supply) * (2 * (supply - 1) + 1) / 6;
-        uint256 sum2 = supply == 0 && amount == 1 ? 0 : (supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) / 6;
-        uint256 summation = sum2 - sum1;
-        return summation * 1 ether / 16000;
+    function shouldTakeFee(address sender, address recepient) internal view returns (bool) {
+        return sender == routerAdress || recepient == routerAdress || (!isFeeExcluded[sender] && !isFeeExcluded[recepient]);
     }
 
-    function getBuyPrice(address sharesSubject, uint256 amount) public view returns (uint256) {
-        return getPrice(sharesSupply[sharesSubject], amount);
+    function takeFee(address sender, uint256 amount) internal returns (uint256) {
+        uint256 feeAmount = amount.mul(totalFee).div(feeDenominator);
+        _balances[address(this)] = _balances[address(this)].add(feeAmount);
+        emit Transfer(sender, address(this), feeAmount);
+        return amount.sub(feeAmount);
     }
 
-    function getSellPrice(address sharesSubject, uint256 amount) public view returns (uint256) {
-        return getPrice(sharesSupply[sharesSubject] - amount, amount);
+    function shouldSwapBack() internal view returns (bool) {
+        return msg.sender != pair
+        && !inSwap
+        && swapEnabled
+        && _balances[address(this)] >= swapThreshold;
     }
 
-    function getBuyPriceAfterFee(address sharesSubject, uint256 amount) public view returns (uint256) {
-        uint256 price = getBuyPrice(sharesSubject, amount);
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        uint256 subjectFee = price * subjectFeePercent / 1 ether;
-        return price + protocolFee + subjectFee;
+    function swapBack() internal swapping {
+        uint256 contractTokenBalance = swapThreshold;
+        uint256 amountToLiquify = contractTokenBalance.mul(liquidityFee).div(totalFee).div(2);
+        uint256 amountToSwap = contractTokenBalance.sub(amountToLiquify);
+
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = router.WETH();
+
+        uint256 balanceBefore = address(this).balance;
+
+        router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            amountToSwap,
+            0,
+            path,
+            address(this),
+            block.timestamp
+        );
+        uint256 amountETH = address(this).balance.sub(balanceBefore);
+        uint256 totalETHFee = totalFee.sub(liquidityFee.div(2));
+        uint256 amountETHLiquidity = amountETH.mul(liquidityFee).div(totalETHFee).div(2);
+        uint256 amountETHMarketing = amountETH.mul(marketingFee).div(totalETHFee);
+
+
+        (bool MarketingSuccess, /* bytes memory data */) = payable(marketingFeeReceiver).call{value : amountETHMarketing, gas : 30000}("");
+        require(MarketingSuccess, "receiver rejected ETH transfer");
+
+        if (amountToLiquify > 0) {
+            router.addLiquidityETH{value : amountETHLiquidity}(
+                address(this),
+                amountToLiquify,
+                0,
+                0,
+                DEAD,
+                block.timestamp
+            );
+            emit AutoLiquify(amountETHLiquidity, amountToLiquify);
+        }
     }
 
-    function getSellPriceAfterFee(address sharesSubject, uint256 amount) public view returns (uint256) {
-        uint256 price = getSellPrice(sharesSubject, amount);
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        uint256 subjectFee = price * subjectFeePercent / 1 ether;
-        return price - protocolFee - subjectFee;
+    function buyTokens(uint256 amount, address to) internal swapping {
+        address[] memory path = new address[](2);
+        path[0] = router.WETH();
+        path[1] = address(this);
+
+        router.swapExactETHForTokensSupportingFeeOnTransferTokens{value : amount}(
+            0,
+            path,
+            to,
+            block.timestamp
+        );
     }
 
-    function buyShares(address sharesSubject, uint256 amount) public payable {
-        uint256 supply = sharesSupply[sharesSubject];
-        require(supply > 0 || sharesSubject == msg.sender, "Only the shares' subject can buy the first share");
-        uint256 price = getPrice(supply, amount);
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        uint256 subjectFee = price * subjectFeePercent / 1 ether;
-        require(msg.value >= price + protocolFee + subjectFee, "Insufficient payment");
-        sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] + amount;
-        sharesSupply[sharesSubject] = supply + amount;
-        emit Trade(msg.sender, sharesSubject, true, amount, price, protocolFee, subjectFee, supply + amount);
-        (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success2, ) = sharesSubject.call{value: subjectFee}("");
-        require(success1 && success2, "Unable to send funds");
+    function clearStuckBalance() external {
+        payable(marketingFeeReceiver).transfer(address(this).balance);
     }
 
-    function sellShares(address sharesSubject, uint256 amount) public payable {
-        uint256 supply = sharesSupply[sharesSubject];
-        require(supply > amount, "Cannot sell the last share");
-        uint256 price = getPrice(supply - amount, amount);
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        uint256 subjectFee = price * subjectFeePercent / 1 ether;
-        require(sharesBalance[sharesSubject][msg.sender] >= amount, "Insufficient shares");
-        sharesBalance[sharesSubject][msg.sender] = sharesBalance[sharesSubject][msg.sender] - amount;
-        sharesSupply[sharesSubject] = supply - amount;
-        emit Trade(msg.sender, sharesSubject, false, amount, price, protocolFee, subjectFee, supply - amount);
-        (bool success1, ) = msg.sender.call{value: price - protocolFee - subjectFee}("");
-        (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success3, ) = sharesSubject.call{value: subjectFee}("");
-        require(success1 && success2 && success3, "Unable to send funds");
+    function clearStuckTBalance() external {
+        _basicTransfer(address(this), marketingFeeReceiver, balanceOf(address(this)));
     }
+
+    function setSwapThreshold(uint256 _swapThreshold) external onlyOwner {
+        swapThreshold = _totalSupply / 100000 * _swapThreshold;
+    }
+
+    function changeMaxWalletAmount(uint8 maxAmountPercent) public onlyOwner {
+        require(maxAmountPercent > 0, "Max wallet amount should be greater then 1%");
+        _maxWalletAmount = (_totalSupply * maxAmountPercent) / 1000;
+    }
+
+    function addFeeExcluded(address wallet) public onlyOwner {
+        isFeeExcluded[wallet] = true;
+    }
+
+    function addIsBagSizeLimitExcluded(address wallet) public onlyOwner {
+        isBagSizeLimitExcluded[wallet] = true;
+    }
+
+    event AutoLiquify(uint256 amountETH, uint256 amountBOG);
 }
